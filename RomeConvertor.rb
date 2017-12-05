0cd6ad783b18b5d584d05ed1360edc8f15d23cf4
FireStorm2000 <teenwolf849@gmail.com>
2017-12-05 23:12:17 +0200

    ujas

--- RomeConvertor.rb
+++ RomeConvertor.rb
@@ -6,31 +6,15 @@ VALUES = [
   ["C", 100], 
   ["L", 50], 
   ["X", 10], 
-  ["V", 5], 
+  ["V", 5],
+  ["IV", 4], 
+  ["CM", 900 ],  
+  ["CD", 400], 
+  ["XC", 90],  
   ["I", 1], 
+  ["XL", 40],
+ ["IX", 9],
 ]
-class String 
-  private
- 
-  def roman_mapping
-    {
-      1000 => "M",
-      900 => "CM",
-      500 => "D",
-      400 => "CD",
-      100 => "C",
-      90 => "XC",
-      50 => "L",
-      40 => "XL",
-      10 => "X",
-      9 => "IX",
-      5 => "V",
-      4 => "IV",
-      1 => "I"
-    }
-  end
-end
-
 class RomeConvertor
 
 def to_rome int_arg
